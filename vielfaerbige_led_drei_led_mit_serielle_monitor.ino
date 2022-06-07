10c59d6b4e93f31e94466055a45e67e0fc42b96d
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-06-07 08:59:58 +0200

    07062022

--- vielfaerbige_led_drei_led_mit_serielle_monitor.ino
+++ vielfaerbige_led_drei_led_mit_serielle_monitor.ino
@@ -1,45 +1,33 @@
-int drei_led_rot = 3;
-int drei_led_gruen = 5;
-int drei_led_blau = 6;
+String farben[] = {"R", "G", "B"};
+int leds[] = {3,5,6};
+String value_s_monitor[3];
 int value[3];
-int value_rot; 
-int value_gruen;
-int value_blau;
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
-  Serial.println("Bitte gewünschte Kombination eingeben: rot, grün, blau");
-  pinMode(drei_led_rot,OUTPUT);
-  pinMode(drei_led_gruen,OUTPUT);
-  pinMode(drei_led_blau, OUTPUT);
+  Serial.println("Bitte gewünschte Kombination eingeben:");
+  for (int i = 0; i < 3; i++) {
+    pinMode(leds[i],OUTPUT);
+  }
+  
 }
 
 void loop() {
-  analogWrite(drei_led_blau, 200);
-    if (Serial.available()) {
-      while (Serial.available()) {
-        
-        value[0] = Serial.parseInt();
-        Serial.println("Rot");
-        Serial.println(value[0]);
-        analogWrite(drei_led_rot, value[0]);
-        delay(2000);
-        
-        value[1] = Serial.parseInt();
-        Serial.println("Gruen");
-        Serial.println(value[1]);
-        analogWrite(drei_led_gruen, value[1]);
-        delay(2000);
+
   
-        value[2] = Serial.parseInt();
-        Serial.println("Blau");
-        Serial.println(value[2]);
-        analogWrite(drei_led_blau, value[2]);
-        delay(2000);
-        
-        delay(10000);
+  for (int i = 0; i < 3; i++) {
+    if (Serial.available()) {
+      while (Serial.available()) {  
+        value_s_monitor[i] = Serial.readStringUntil('\n');
+        value[i] = value_s_monitor[i].toInt();
+        analogWrite(leds[i], value[i]);
+        Serial.println(farben[i]);
+        Serial.println(value[i]);
+        delay(1000);
       }
     }
   }
+  
+}
   
