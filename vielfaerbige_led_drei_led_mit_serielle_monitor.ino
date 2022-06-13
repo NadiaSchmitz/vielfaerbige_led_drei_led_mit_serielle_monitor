e9da3713f02f3c823675f7ece8ff1e083d87310c
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-06-13 11:37:13 +0200

    13062022

--- vielfaerbige_led_drei_led_mit_serielle_monitor.ino
+++ vielfaerbige_led_drei_led_mit_serielle_monitor.ino
@@ -1,32 +1,39 @@
-String farben[] = {"R", "G", "B"};
 int leds[] = {3,5,6};
-String value_s_monitor[3];
-int value[3];
+int leds_value[3];
 
 void setup() {
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("Bitte gewünschte Kombination eingeben:");
   for (int i = 0; i < 3; i++) {
     pinMode(leds[i],OUTPUT);
   }
-  
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  delay(1000);
+  Serial.println("Bitte ein Zahl zwischen 1 und 10");
 }
 
 void loop() {
-
-  
-  for (int i = 0; i < 3; i++) {
-    if (Serial.available()) {
+  if (Serial.available()) {
       while (Serial.available()) {  
-        value_s_monitor[i] = Serial.readStringUntil('\n');
-        value[i] = value_s_monitor[i].toInt();
-        analogWrite(leds[i], value[i]);
-        Serial.println(farben[i]);
-        Serial.println(value[i]);
-        delay(1000);
-      }
+        int control_sum;
+        int value = Serial.parseInt();
+        delay(30);
+        Serial.println(value);
+        
+        for (int i = 0; i < 3; i++) {
+          
+          control_sum = random(200);
+          Serial.println(control_sum);
+          leds_value[i] = value * control_sum;
+          if (leds_value[i] > 255) {
+            leds_value[i] = 255;
+          }
+          Serial.println(leds_value[i]);
+          analogWrite(leds[i], leds_value[i]);
+          
+        }
+        delay(100000);
     }
+    
   }
   
 }
